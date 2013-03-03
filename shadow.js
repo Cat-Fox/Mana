60d56d48dbe1cd9be6d7cdb775860b3f56b978f3
Zaraka <zarakaangel@gmail.com>
2013-03-03 03:09:53 +0100

    cleanup

--- shadow.js
+++
@@ -1,9 +0,0 @@
-var Shadow = me.ObjectEntity.extend ({
-    init: function(x, y){
-        settings = {};
-        settings.image = "shadow16";
-        settings.spritewidth = 16;
-        settings.spriteheight = 16;
-        this.parent(x, y, settings);
-    } 
-});
