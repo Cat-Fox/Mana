869d74d3137d7ff2d95552429b96f26f940541d2
zaraka <zaraka@Zaraka-laptop.lan>
2013-03-20 14:57:49 +0100

    talking npc

--- guard.js
+++ guard.js
@@ -4,6 +4,7 @@ var Guard = me.ObjectEntity.extend({
     hp: 100,
     target: null,
     shadow: null,
+    use: null,
     init: function(x, y, settings) {
         this.parent(x, y, settings);
         this.collidable = true;
@@ -19,29 +20,59 @@ var Guard = me.ObjectEntity.extend({
     update: function() {
         var targeted = false;
         
-        var res = me.game.collide(this);
-        if (res) {         
-            if (res.obj.type === "human_target") {
-                targeted = true;
-                me.game.HUD.setItemValue("EnemyHP", this.hp + ";guard;ally");
-                if (this.target === null) {
-                    this.target = new Target(this.pos.x + 6, this.pos.y + 8, "green");
-                    me.game.add(this.target, 3);
-                    me.game.sort();
+        var res = me.game.collide(this, true);
+        if (res.length >= 1) {
+            for (var i = 0; i< res.length; i++) {
+                if (res[i].obj.type === "human_target") {
+                    targeted = true;
+                    me.game.HUD.setItemValue("EnemyHP", this.hp + ";guard;ally");
+                    if (this.target === null) {
+                        this.target = new Target(this.pos.x + 6, this.pos.y + 8, "green");
+                        me.game.add(this.target, 3);
+                        me.game.sort();
+                    }
+                }
+                if (res[i].obj.type === "human_use") {
+                    if (this.use === null) {
+                        console.log("guard_use");
+                        var text = "";
+                        var rand = Math.floor(Math.random()*6);
+                        switch(rand){
+                            case 0: text = "Ondro divej se do sve obrazovky";
+                                break;
+                            case 1: text = "Hello traveler!";
+                                break;
+                            case 2: text = "Ololol";
+                                break;
+                            case 3: text = "Javascript sucks, but\n dont tell the programmer";
+                                break;
+                            case 4: text = "If you kill me, you suck";
+                                break;
+                            case 5: text = "IVH is boooooooring";
+                                break
+                        }
+                        console.log(rand + " " + text);
+                        this.use = new Message(this.pos.x - 25, this.pos.y - 40, text);
+                        me.game.add(this.use, 6);
+                        me.game.sort();
+                    }
                 }
             }
         }
-        
-        if(targeted === false && this.target !== null){
+
+        if (targeted === false && this.target !== null) {
             me.game.remove(this.target);
             this.target = null;
+            if (this.use !== null) {
+                me.game.remove(this.use);
+                this.use = null;
+            }
         }
 
         this.parent();
         return true;
     },
     onDestroyEvent: function() {
-        me.input.releaseMouseEvent('mousemove', this.collisionBox);
         //shadow will always be created, but target is dependent on update function
         me.game.remove(this.shadow);
         //me.game.remove(this.target);
