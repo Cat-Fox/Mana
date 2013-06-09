3df70adc05f4b411e25309b1fc8d3905f5127393
zaraka <zaraka@Zaraka-laptop.lan>
2013-06-09 15:31:46 +0200

    migrated resources

---
+++ resources.js
@@ -0,0 +1,130 @@
+// game resources
+    game.resources = [{
+    name: "tilesheet",
+    type: "image",
+    src: "data/tilesheet.png"
+}, {
+    name: "test_map",
+    type: "tmx",
+    src: "data/maps/test_map.tmx"
+}, {
+    name: "house",
+    type: "tmx",
+    src: "data/maps/house.tmx"
+},  {
+    name: "human",
+    type: "image",
+    src: "data/human.png"
+}, {
+    name: "human_up",
+    type: "image",
+    src: "data/human_up.png"
+}, {
+    name: "human_right",
+    type: "image",
+    src: "data/human_right.png"
+}, {
+    name: "human_down",
+    type: "image",
+    src: "data/human_down.png"
+}, {
+    name: "burger",
+    type: "image",
+    src: "data/burger.png"
+}, {
+    name: "item-sword1",
+    type: "image",
+    src: "data/item-sword1.png"
+}, {
+    name: "sword1",
+    type: "image",
+    src: "data/sword1.png"
+}, {
+    name: "target",
+    type: "image",
+    src: "data/target.png"
+}, {
+    name: "npc_guard",
+    type: "image",
+    src: "data/npc_guard.png"
+}, {
+    name: "npc_rat",
+    type: "image",
+    src: "data/npc_rat.png"
+}, {
+    name: "shadow16",
+    type: "image",
+    src: "data/shadow16.png"
+}, {
+    name: "smiles16",
+    type: "image",
+    src: "data/smiles16.png"
+},{
+    name: "sparks",
+    type: "image",
+    src: "data/sparks.png"
+},{
+    name: "inventory_tile",
+    type: "image",
+    src: "data/inventory_tile.png"
+},{
+    name: "backpack",
+    type: "image",
+    src: "data/backpack.png"
+},{
+    name: "death",
+    type: "image",
+    src: "data/death.png"
+},{
+    name: "message",
+    type: "image",
+    src: "data/message.png"
+},{
+    name: "particle",
+    type: "image",
+    src: "data/particle.png"
+},{
+    name: "particle2",
+    type: "image",
+    src: "data/particle2.png"
+},{
+    name: "particle3",
+    type: "image",
+    src: "data/particle3.png"
+},{
+    name: "8x8Font",
+    type: "image",
+    src: "data/fonts/8x8Font-white.png"
+},{
+    name: "geebeeyay-8x8",
+    type: "image",
+    src: "data/fonts/geebeeyay-8x8.png"
+},{
+    name: "gold_8x8",
+    type: "image",
+    src: "data/fonts/gold_8x8.png"
+}, {
+    name: "itempick2",
+    type: "audio",
+    src: "data/sounds/"
+}, {
+    name: "metal-clash",
+    type: "audio",
+    src: "data/sounds/"
+}, {
+    name: "exp_click",
+    type: "audio",
+    src: "data/sounds/"
+}, {
+    name: "fire",
+    type: "audio",
+    src: "data/sounds/"
+}, {
+    name: "level_up",
+    type: "audio",
+    src: "data/sounds/"
+}, {
+    name: "swing",
+    type: "audio",
+    src: "data/sounds/"
+}];
