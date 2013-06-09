56af725fddb52a9e2c5653c9db7af5492e0fe458
zaraka <zaraka@Zaraka-laptop.lan>
2013-06-09 15:33:05 +0200

    new library version

--- guard.js
+++ guard.js
@@ -1,4 +1,4 @@
-var Guard = me.ObjectEntity.extend({
+game.Guard = me.ObjectEntity.extend({
     sparks: null,
     time: null,
     hp: 100,
@@ -6,14 +6,15 @@ var Guard = me.ObjectEntity.extend({
     shadow: null,
     use: null,
     init: function(x, y, settings) {
+        console.log("creating guard");
         this.parent(x, y, settings);
         this.collidable = true;
-        this.addAnimation("idle", [0, 1], 35);
-        this.setCurrentAnimation("idle");
+        this.renderable.addAnimation("idle", [0, 1], 35);
+        this.renderable.setCurrentAnimation("idle");
         this.type = "npc";
         this.updateColRect(5, 17, 2, 18);
 
-        this.shadow = new Shadow(this.pos.x + 6, this.pos.y + 8);
+        this.shadow = new game.Shadow(this.pos.x + 6, this.pos.y + 8);
         me.game.add(this.shadow, 4);
         me.game.sort();
     },
@@ -25,9 +26,10 @@ var Guard = me.ObjectEntity.extend({
             for (var i = 0; i< res.length; i++) {
                 if (res[i].obj.type === "human_target") {
                     targeted = true;
-                    me.game.HUD.setItemValue("EnemyHP", this.hp + ";guard;ally");
+                    //TODO:CHANGE
+                    //me.game.HUD.setItemValue("EnemyHP", this.hp + ";guard;ally");
                     if (this.target === null) {
-                        this.target = new Target(this.pos.x + 6, this.pos.y + 8, "green");
+                        this.target = new game.Target(this.pos.x + 6, this.pos.y + 8, "green");
                         me.game.add(this.target, 3);
                         me.game.sort();
                     }
@@ -52,7 +54,7 @@ var Guard = me.ObjectEntity.extend({
                                 break
                         }
                         console.log(rand + " " + text);
-                        this.use = new Message(this.pos.x - 25, this.pos.y - 40, text);
+                        this.use = new game.Message(this.pos.x - 25, this.pos.y - 40, text);
                         me.game.add(this.use, 6);
                         me.game.sort();
                     }
@@ -79,7 +81,7 @@ var Guard = me.ObjectEntity.extend({
     },
     createSparks: function() {
         if (this.sparks === null) {
-            this.sparks = new Sparks(this.pos.x, this.pos.y);
+            this.sparks = new game.Sparks(this.pos.x, this.pos.y);
             me.game.add(this.sparks, 4);
             me.game.sort();
             this.time = me.timer.getTime();
